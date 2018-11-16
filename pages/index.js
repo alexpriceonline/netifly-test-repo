4bceb148bc45b06c58cea346027bd8b9d63ea8a5
Alex Price <hi@alexpriceonline.com>
2018-11-16 11:39:05 +0000

    Added stripe checkout test

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const onToken = token => {
+  console.log("hit");
+  fetch("/save-stripe-token", {
+    method: "POST",
+    body: JSON.stringify(token)
+  }).then(response => {
+    response.json().then(data => {
+      alert(`We are in business, ${data.email}`);
+    });
+  });
+};
 
 export default () => {
   // Declare a new state variable, which we'll call "count"
@@ -8,6 +21,15 @@ export default () => {
     <div>
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>Click me</button>
+
+      <StripeCheckout
+        token={onToken}
+        stripeKey="pk_test_mWZk6aPfUsPtg5WtJ5wwDhy0"
+        name="ThoughtfulSMS"
+        description="Weekly thoughtful messages via SMS"
+        amount={200}
+        currency="GBP"
+      />
     </div>
   );
 };
